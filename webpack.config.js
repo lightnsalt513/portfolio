9857ff3fea413c99089c459269f86e0b9458ae38
도재경 <jaydo1204@gmail.com>
2022-01-22 23:24:35 +0900

    chore: webpack setting 변경

--- webpack.config.js
+++ webpack.config.js
@@ -3,100 +3,102 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDevelopment = true;
+module.exports = (env, arg) => {
+  const isDevelopment = arg.mode === 'development';
 
-module.exports = {
-  entry: {
-    main: './src/index.ts'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node-modules/
-      },
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src/js')
-        ],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+  return {
+    entry: {
+      main: './src/index.ts'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node-modules/
+        },
+        {
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, 'src/js')
+          ],
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          test: /\.module\.s(a|c)ss$/,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /\.module.(s(a|c)ss)$/,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // { loader: "css-modules-typescript-loader"},
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  mode: 'local',
+                  localIdentName: "[name]__[local]--[hash:base64:5]",
+                },
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i, 
+          type: 'asset/resource',
         }
-      },
-      {
-        test: /\.module\.s(a|c)ss$/,
-        use: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-        ]
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        use: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          // { loader: "css-modules-typescript-loader"},
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                mode: 'local',
-                localIdentName: "[name]__[local]--[hash:base64:5]",
-              },
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i, 
-        type: 'asset/resource',
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CleanWebpackPlugin()
-  ],
-  devServer: {
-    port: 9000
-  },
-  devtool: 'source-map',
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CleanWebpackPlugin()
+    ],
+    devServer: {
+      port: 9000
+    },
+    devtool: 'source-map',
+  }
 }
