aa6a0ba041736380d0c476f4baf3518a668883b5
도재경 <jaydo1204@gmail.com>
2022-02-22 19:16:59 +0900

    fix: 텍스트 수정

--- src/models/ProjectsModel.ts
+++ src/models/ProjectsModel.ts
@@ -19,7 +19,7 @@ export class ProjectsModel {
     {
       title: 'Watch Configurator (React version)',
       description:
-        '기존 진행했던 jQuery 기반의 프로젝트를 React와 TypeScript를 활용하여 부분 재구현',
+        '기존 진행했던 jQuery 기반의 반응형 프로젝트를 React와 TypeScript를 활용하여 부분 재구현',
       tags: ['React', 'Redux', 'TypeScript', 'SASS'],
       imgUrl: thumb1,
       github: 'https://github.com/lightnsalt513/configurator-react',
@@ -28,7 +28,7 @@ export class ProjectsModel {
     {
       title: 'Portfolio Site',
       description:
-        'JavaScript(TypeScript)만을 활용하여 스태틱한 포트폴리오 사이트를 컴포넌트 방식으로 개발',
+        'JavaScript(TypeScript)만을 활용하여 스태틱한 반응형 포트폴리오 사이트를 컴포넌트 방식으로 개발',
       tags: ['TypeScript', 'SASS'],
       imgUrl: thumb2,
       github: 'https://github.com/lightnsalt513/portfolio',
@@ -37,7 +37,7 @@ export class ProjectsModel {
     {
       title: 'E-Commerce',
       description:
-        'MERN 스택으로 간단한 이커머스 사이트 기능을 구현해 보며 DB부터 서버, 프론트까지의 주요 개발 흐름 학습',
+        'MERN 스택으로 간단한 이커머스 반응형 사이트 기능을 구현해 보며 DB부터 서버, 프론트까지의 주요 개발 흐름 학습',
       tags: ['React', 'Redux', 'Styled-Components', 'Express', 'MongoDB'],
       imgUrl: thumb3,
       github: 'https://github.com/lightnsalt513/shop-mern',
@@ -54,7 +54,7 @@ export class ProjectsModel {
     {
       title: 'Canvas R&amp;D',
       description:
-        '스크롤 기반 텍스트 애니메이션을 Canvas API를 활용해 구현한 R&amp;D 프로젝트',
+        '스크롤 기반 텍스트 애니메이션을 Canvas API를 활용해 구현한 R&amp;D 프로젝트 (반응형)',
       tags: ['JavaScript', 'jQuery', 'TweenMax', 'ScrollMagic'],
       imgUrl: thumb5,
       github: 'https://github.com/lightnsalt513/canvas-rnd',
@@ -63,7 +63,7 @@ export class ProjectsModel {
     {
       title: 'Watch Configurator',
       description:
-        '갤럭시 워치와 여러 밴드를 결합해보며 상품을 고를 수 있는, 애니메이션 효과 중심의 프로모셔널 컴포넌트 개발 <br /> (보안 등의 이슈로 Github에 소스코드는 포함되어 있지 않습니다. 작업내용은 README를 참고해 주세요.)',
+        '갤럭시 워치와 여러 밴드를 결합해보며 상품을 고를 수 있는, 애니메이션 효과 중심의 프로모셔널 반응형 컴포넌트 개발 <br /> (보안 등의 이슈로 Github에 소스코드는 포함되어 있지 않습니다. 작업내용은 README를 참고해 주세요.)',
       tags: ['JavaScript', 'jQuery', 'SASS', 'TweenMax'],
       imgUrl: thumb6,
       github: 'https://github.com/lightnsalt513/configurator-jquery',
--- src/views/AboutView.ts
+++ src/views/AboutView.ts
@@ -17,13 +17,13 @@ export class AboutView {
           공감을 기반으로하는 소통과 협업을 중시하는 Front-end 개발자 도재경입니다.
         </p>
         <p>
-          대기업 글로벌 웹사이트 구축/운영 프로젝트 경험을 통해 다국어, 반응형, 웹접근성 대응 전문성을 쌓았으며,
+          대기업 글로벌 웹사이트 구축/운영 프로젝트 경험을 통해 다국어, 반응형, 웹접근성 대응 역량을 쌓았으며,
           체계적이고 투명한 일정관리 및 커뮤니케이션 역량을 인정받아 다수의 프로젝트에서 마크업 리드로도 참여했습니다.
         </p>
         <p>
           공동의 목표를 가진 다양한 동료들과 유기적 협업을 통해 결과물을 만드는 일에 기쁨을 느끼며,
           새로운 환경과 상황에 기민하게 적응하고 도전적인 자세로 임합니다.
-          퍼블리셔로 전향한 이후 4년 반 동안 꾸준한 자기 역량계발을 통해 JavaScript 개발자로까지 성장했습니다.
+          퍼블리셔로 전향한 이후 4년 반 동안 꾸준한 자기 역량계발을 통해 JavaScript 개발자로까지 성장했고, 현재는 JavaScript의 코어 개념과 더불어 React, TypeScript 등의 최신 FE 기술을 학습하고 있습니다.
           그리고 지금까지의 경험들을 기반으로 더 크게 도전하고 함께 성장할 수 있는 환경에서 멋진 FE 개발자가 되길 희망합니다. 
         </p>
         <div class=${compStyle.about__detail}>
@@ -39,7 +39,7 @@ export class AboutView {
           </ul>
           <dl class=${compStyle['about__detail-skills']}>
             <div>
-              <dt>Web</dt>
+              <dt>HTML</dt>
               <dd><em style="width:90%"><span>90%</span></em></dd>
             </div>
             <div>
@@ -56,7 +56,7 @@ export class AboutView {
             </div>
             <div>
               <dt>TypeScript</dt>
-              <dd><em style="width:50%"><span>50%</span></em></dd>
+              <dd><em style="width:35%"><span>35%</span></em></dd>
             </div>
           </dl>
         </div>
